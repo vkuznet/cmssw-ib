f054ecdd4890e4668cb26556dab8c55f46df5225
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-04-25 17:56:40 +0200

    CMSSW_6_2_X_2013-04-05-0200

---
+++ Documentation/DataFormats/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Documentation/DataFormats/',1)[0])+'/cfipython/slc6_amd64_gcc480/Documentation/DataFormats')
--- RecoMET/METProducers/src/SealModule.cc
+++ RecoMET/METProducers/src/SealModule.cc
@@ -8,12 +8,15 @@
 #include "RecoMET/METProducers/interface/HcalHaloDataProducer.h" 
 #include "RecoMET/METProducers/interface/EcalHaloDataProducer.h" 
 #include "RecoMET/METProducers/interface/GlobalHaloDataProducer.h" 
+#include "RecoMET/METProducers/interface/ParticleFlowForChargedMETProducer.h" 
+
 using cms::METProducer;
 using reco::BeamHaloSummaryProducer;
 using reco::CSCHaloDataProducer;
 using reco::HcalHaloDataProducer;
 using reco::EcalHaloDataProducer;
 using reco::GlobalHaloDataProducer;
+using reco::ParticleFlowForChargedMETProducer;
 
 DEFINE_FWK_MODULE(METProducer);
 DEFINE_FWK_MODULE(BeamHaloSummaryProducer);
@@ -21,6 +24,7 @@ DEFINE_FWK_MODULE(CSCHaloDataProducer);
 DEFINE_FWK_MODULE(HcalHaloDataProducer);
 DEFINE_FWK_MODULE(EcalHaloDataProducer);
 DEFINE_FWK_MODULE(GlobalHaloDataProducer);
+DEFINE_FWK_MODULE(ParticleFlowForChargedMETProducer);
 
 #include "RecoMET/METProducers/interface/MuonMET.h"
 using cms::MuonMET;
