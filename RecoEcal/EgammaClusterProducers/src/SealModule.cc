fedffcf724be328bfaec90385a72c85d2111d872
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-04-25 18:48:04 +0200

    CMSSW_6_2_X_2013-04-13-1400

--- RecoEcal/EgammaClusterProducers/src/SealModule.cc
+++ RecoEcal/EgammaClusterProducers/src/SealModule.cc
@@ -44,3 +44,4 @@ DEFINE_FWK_MODULE(UnifiedSCCollectionProducer);
 DEFINE_FWK_MODULE(CleanAndMergeProducer);
 DEFINE_FWK_MODULE(PFECALBoxSuperClusterProducer);
 DEFINE_FWK_MODULE(PreshowerPhiClusterProducer);
+
